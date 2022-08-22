75a9076d4e1c557b61b2362a107e2e3edfe5517f
Crypto and Coffee <null@cryptoandcoffee.com>
2022-08-22 10:40:56 -0700

    Update defaults and better CPU support

--- pkt-miner/run.sh
+++ pkt-miner/run.sh
@@ -14,6 +14,15 @@ fi
 CPU_COUNT=$(echo "scale=0; $CPU_COUNT/100000" | bc -l) #Convert to threads
 echo "Found $CPU_COUNT cpus available."
 
+
+
+#Set uploaders to CPU count if higher than default 10
+if [[ $CPU_COUNT < 1 ]]; then
+THREADS=""
+else
+THREADS="--threads $CPU_COUNT"
+fi
+
 #Set uploaders to CPU count if higher than default 10
 if [[ $CPU_COUNT > 10 ]]; then
 UPLOADERS=$CPU_COUNT
@@ -38,8 +47,8 @@ echo "using pools ${pools[*]}..."
 if [[ $VERSION != "v0.5.2" ]]; then
 echo "Got version $VERSION, pulling from Github" ; rm packetcrypt-${VERSION}-linux_amd64
 wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-${VERSION}/packetcrypt-${VERSION}-linux_amd64 ; chmod +x packetcrypt-${VERSION}-linux_amd64
-./packetcrypt-$VERSION-linux_amd64 ann --paymentaddr "$WALLET_ADDR" "${pools[@]}" --threads $CPU_COUNT --uploaders $UPLOADERS
+./packetcrypt-$VERSION-linux_amd64 ann --paymentaddr "$WALLET_ADDR" "${pools[@]}" --uploaders $UPLOADERS $THREADS
 else
 echo "Got default version $VERSION"
-./packetcrypt-$VERSION-linux_amd64 ann --paymentaddr "$WALLET_ADDR" "${pools[@]}" --threads $CPU_COUNT --uploaders $UPLOADERS
+./packetcrypt-$VERSION-linux_amd64 ann --paymentaddr "$WALLET_ADDR" "${pools[@]}" --uploaders $UPLOADERS $THREADS
 fi
